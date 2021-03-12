4949cf14691fb66ded86af1ad41e7472981c96b3
Christian Antonio <cantonio@kth.se>
2021-03-12 11:11:38 +0100

    change api base adress

--- src/components/api.js
+++ src/components/api.js
@@ -6,7 +6,7 @@
  */
 const ApiCall = (httpMethod, location, JSONdata, authToken) => {
 
-  return fetch("http://localhost:8000/" + location, {
+  return fetch("http://recruitmebackend.herokuapp.com" + location, {
     method: httpMethod,
     headers: {
       "Content-Type":"application/json",
